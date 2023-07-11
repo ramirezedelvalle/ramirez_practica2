8253a3c612a0914e5bf272e157dc234f52f0dc63
Nozen <ramirezedelvalle@gmail.com>
2023-07-10 20:05:01 -0600

    Subida de Documento Final

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practica No. 2</title>
+</head>
+<body>
+    <h1>Practica No. 2</h1>
+    <label for="textBox">Ingrese un texto:</label>
+    <input type="text" id="textBox">
+    <p id="output"></p>
+    <script src="script.js"></script>
+</body>
+</html>
